ab25d0c74aa0635d00d884da3a489c91e6b55798
Jaime Feldman <jaime.feldman@mac.com>
2024-02-07 02:02:33 -0300

    refactor(json_loader): ahora filtra y busca en elarreglo
    Filtra y busca en los arreglos destacando cualquier coicidencia en color rojo

--- json_loader.py
+++ json_loader.py
@@ -1,5 +1,9 @@
 import json, random
 
+from rich.console import Console
+
+console = Console()
+
 # Lee el archivo JSON
 with open('nombres.json', 'r') as file:
     data = json.load(file)
@@ -11,15 +15,34 @@ apellidos_chilenos = data["nombres"]["apellidos"]["chilenos"]
 profeciones = data["nombres"]["profeciones"]
 
 # Imprime los nombres de hombres
-print("Nombres de hombres:")
-for nombre in nombres_hombres:
-    print(nombre)
+# print("Nombres de hombres:")
+# for nombre in nombres_hombres:
+    # print(nombre)
 
-for apellidos in apellidos_chilenos:
-    print(apellidos)
+# for apellidos in apellidos_chilenos:
+    # print(apellidos)
 
-for profecion in profeciones:
-    print(profecion)
+# for profecion in profeciones:
+    # print(profecion)
 
 # Impriemiendo elementos de forma aleatoria.
 print(f"nombre: {''.join(random.choices(nombres_hombres))} apellido: {''.join(random.choices(apellidos_chilenos))}")
+
+# Buscando un elemento.
+
+elemento_a_buscar = "jaime"
+if elemento_a_buscar in nombres_hombres:
+    print(f"{elemento_a_buscar} está en la lista")
+else:
+    print(f"{elemento_a_buscar} no está en la lista")
+
+
+# Buscando conincidencias.
+
+print("[ Buscando coincidencias con 'ja' ]")
+
+for nombre in nombres_hombres:
+    if "ja" in nombre.lower():
+        nombre_resaltado = nombre.lower().replace("ja", "[red]ja[/red]")
+        console.print(nombre_resaltado)
+
